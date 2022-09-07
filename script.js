2d5e93dc97d06c5c07f33d26cf638131a3a582e3
martinperezbarile <martinperezbarile@gmail.com>
2022-09-07 14:59:55 -0300

    v3

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx3tCjiG3XAPaxOwJQPSQYH7ZQi-SiHmL8mGldPYjBUyfGmAX6ZUJxL3lD7COInNmfu/exec'
+const scriptURL = 'https://script.google.com/a/intraway.com/macros/s/AKfycbzeqgT4jZp9Xa0BxZIUQnA4JHQa1dZ-3XsX_waS/exec'
 const form = document.forms['suscripciones']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      console.log('Success!', response);
+      form.reset();
+    })
     .catch(error => console.error('Error!', error.message))
 })
