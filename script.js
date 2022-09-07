add4000f86f3e1a53992c1c3a6d5766645d9ba17
martinperezbarile <martinperezbarile@gmail.com>
2022-09-07 17:45:01 -0300

    v1.5

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyY6ey92XhEOdyQjIYnv0oUQUgD4HUnswSOfH3-OcOlbfezG3I/exec'
-const form = document.forms['suscrip']
+function prueba() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyY6ey92XhEOdyQjIYnv0oUQUgD4HUnswSOfH3-OcOlbfezG3I/exec';
+  const form = document.forms['formulario'];
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      console.log('Success!', response);
-      form.reset();
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        console.log('Success!', response);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+}
