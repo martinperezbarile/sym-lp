f461845b03ec08fa2ad81ab3b88b12e8f0054cf9
martinperezbarile <martinperezbarile@gmail.com>
2022-09-07 15:31:39 -0300

    v1.3

---
+++ script.js
@@ -0,0 +1,17 @@
+const dataForm = document.getElementById('dataForm');
+
+dataForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  fetch('https://sheet.best/api/sheets/2fb982d7-fbd1-4f9c-a605-00ba9d21450a', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "Network element": "Ejemplo de network",
+      "Email": "Ejemplo de email" 
+    })
+  });
+});
