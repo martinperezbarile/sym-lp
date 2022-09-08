c1a50bece1da736455fd23c3ab52c72d9bbe2f94
martinperezbarile <martinperezbarile@gmail.com>
2022-09-07 21:59:34 -0300

    v1.5

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function prueba() {
+function sym() {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbyY6ey92XhEOdyQjIYnv0oUQUgD4HUnswSOfH3-OcOlbfezG3I/exec';
   const form = document.forms['formulario'];
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
       .then(response => {
-        console.log('Success!', response);
         form.reset();
       })
-      .catch(error => console.error('Error!', error.message))
+      .catch(error => console.error('error', error.message))
   })
 }
